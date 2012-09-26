42c33f0f0d530ea5776dd9f7f574a9bd57a77ea7
Christian Holt <ymabob@gmail.com>
2012-09-27 01:20:35 +0200

    Added a main menu
    Added the main menu for land layout. Some changes will have to be made,
    but... progress!

--- BallMazeGame/gen/com/example/ballmazegame/R.java
+++ BallMazeGame/gen/com/example/ballmazegame/R.java
@@ -13,9 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int main_menu=0x7f020002;
+        public static final int main_menu_standing=0x7f020003;
+        public static final int maze1=0x7f020004;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int credits=0x7f070002;
+        public static final int high_score=0x7f070003;
+        public static final int how_to=0x7f070001;
+        public static final int menu_settings=0x7f070005;
+        public static final int quit_game=0x7f070004;
+        public static final int start_game=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -24,12 +32,18 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int Credits=0x7f040006;
+        public static final int High_score=0x7f040007;
+        public static final int How_to_play=0x7f040005;
+        public static final int Quit_game=0x7f040008;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int imageDescription=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int start_game=0x7f040004;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
+        public static final int MenuFont=0x7f050001;
     }
 }
--- BallMazeGame/src/com/example/ballmazegame/MainActivity.java
+++ BallMazeGame/src/com/example/ballmazegame/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.ballmazegame;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	
+	public static Typeface typeface;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_main);
+	    
+	    typeface = Typeface.createFromAsset(getAssets(), "BonvenoCF-Light.otf");
+	    
+	    TextView startGame = (TextView) findViewById(R.id.start_game);
+	    TextView howToPlay = (TextView) findViewById(R.id.how_to);
+	    TextView credits = (TextView) findViewById(R.id.credits);
+	    TextView highScore = (TextView) findViewById(R.id.high_score);
+	    TextView quitGame = (TextView) findViewById(R.id.quit_game);
+	    
+	    startGame.setTypeface(typeface);
+	    howToPlay.setTypeface(typeface);
+	    credits.setTypeface(typeface);
+	    highScore.setTypeface(typeface);
+	    quitGame.setTypeface(typeface);
+	    
+	    
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
     }
 }
