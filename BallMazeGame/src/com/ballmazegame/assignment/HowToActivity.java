4e5f72c2cec018f952c256c885b9becab64edac8
Øyvind Nordli <oe.nordli@gmail.com>
2012-09-29 20:32:45 +0200

    How To Play is done

--- BallMazeGame/src/com/ballmazegame/assignment/HowToActivity.java
+++ BallMazeGame/src/com/ballmazegame/assignment/HowToActivity.java
@@ -1,17 +1,27 @@
 package com.ballmazegame.assignment;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class HowToActivity extends Activity {
 
+	public static Typeface typeface;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to);
+        
+	    typeface = Typeface.createFromAsset(getAssets(), "BonvenoCF-Light.otf");  // Create typeface with Custom font. 
+	    
+	    TextView howToPlayGame1 = (TextView) findViewById(R.id.howToPlayGame1);
+	    TextView howToPlayGame2 = (TextView) findViewById(R.id.howToPlayGame2);
+	    TextView howToPlayGame3 = (TextView) findViewById(R.id.howToPlayGame3);
+	    howToPlayGame1.setTypeface(typeface);
+	    howToPlayGame2.setTypeface(typeface);
+	    howToPlayGame3.setTypeface(typeface);
     }
 
 }
