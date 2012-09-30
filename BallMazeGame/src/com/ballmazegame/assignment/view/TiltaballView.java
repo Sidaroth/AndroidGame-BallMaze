a1d5504310d9a46ad66e2249025e34149b787bfa
Christian Holt <ymabob@gmail.com>
2012-10-01 01:18:33 +0200

    Got the ball moving on screen, will have to be tweaked later

--- BallMazeGame/src/com/ballmazegame/assignment/view/TiltaballView.java
+++ BallMazeGame/src/com/ballmazegame/assignment/view/TiltaballView.java
@@ -9,16 +9,16 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager.LayoutParams;
 
 import com.ballmazegame.assignment.controller.BallController;
 import com.ballmazegame.assignment.model.BallModel;
 
-public class TiltaballView extends Activity implements Observer, SensorEventListener {
+public class TiltaballView extends Activity implements SensorEventListener, Observer {
 
-	private BallModel mBall;
-	private BallController mController;
-	private boolean mInitialized;		
 	private DrawView mDrawView;
+	private boolean mInitialized;	
 	
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
@@ -27,64 +27,59 @@ public class TiltaballView extends Activity implements Observer, SensorEventList
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	     super.onCreate(savedInstanceState);
-	     mDrawView = new DrawView(this);
-	     
+		 requestWindowFeature(Window.FEATURE_NO_TITLE); //hide title bar
+		 //set app to full screen and keep screen on 
+		 getWindow().setFlags(0xFFFFFFFF, LayoutParams.FLAG_FULLSCREEN | LayoutParams.FLAG_KEEP_SCREEN_ON);		
+		 super.onCreate(savedInstanceState); 
+		 
+//		 super.onCreate(savedInstanceState);
 	     
 	     mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-	     mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR); // Linear excludes gravity. 
+	     mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); // Linear excludes gravity. 
 	     
 	     // See comment for onResume();
 	     mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-	         
+	        
 	     mInitialized = false;
 	     
+	     
 	     mDrawView = new DrawView(this);
-	     mBall = new BallModel();
-	     mController = new BallController(mBall);
-	     
-	     mBall.addObserver(this);
-	     
+	     mDrawView.mController.mBallModel.addObserver(this);
+	       
 	     setContentView(mDrawView);
 	 }
 	     
 	
+    public void update(Observable observable, Object data) {
+		// TODO Auto-generated method stub
+		mDrawView.invalidate();
+	}
+	
+	
+	// This is called each time the sensor gets new data. 
      public void onSensorChanged(SensorEvent event) {
     	 currX = event.values[0];
     	 currY = event.values[1];
     	 
-		 
+ 		
     	 if ( !mInitialized )
     	 {
-    		 lastX = currX;
-    		 lastY = currY;
-    		 mInitialized = true;
-    		 
+    		 mInitialized = true;  		 
     		 // Rearrange coordinate system here? 
     	 }
     	 else
     	 {
-    		 mController.moveBall(currX, currY);
+  		 	mDrawView.mController.moveBall(currX, currY);
     	 }
      }
-
-    
-     // How to get drawview to update the position without replicating the model?...
-	public void update(Observable observable, Object data) {
-		// ÆSJ!
-		mDrawView.updateBall(mBall);
-		
-		// This is as it should be. 
-		mDrawView.invalidate();
-		
-	}
+ 	
 
 
+     
+     
 	
 	// Accuracy will not be changed during runtime, hence empty. 
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	
