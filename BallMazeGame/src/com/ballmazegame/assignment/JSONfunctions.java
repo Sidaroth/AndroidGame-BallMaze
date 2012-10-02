f0a2daa81aae4a9ef7eba5f1e1847ece743ff99a
Christian Holt <ymabob@gmail.com>
2012-10-02 18:34:28 +0200

    Small fix

--- BallMazeGame/src/com/ballmazegame/assignment/JSONfunctions.java
+++ BallMazeGame/src/com/ballmazegame/assignment/JSONfunctions.java
@@ -3,15 +3,12 @@ package com.ballmazegame.assignment;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
