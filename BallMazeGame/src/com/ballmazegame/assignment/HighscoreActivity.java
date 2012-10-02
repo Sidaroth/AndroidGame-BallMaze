479a6e883adf5d9620c8a6f55adaa8d3dc986718
Øyvind Nordli <oe.nordli@gmail.com>
2012-10-02 15:07:56 +0200

    created Json object

--- BallMazeGame/src/com/ballmazegame/assignment/HighscoreActivity.java
+++ BallMazeGame/src/com/ballmazegame/assignment/HighscoreActivity.java
@@ -1,5 +1,13 @@
 package com.ballmazegame.assignment;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -9,6 +17,27 @@ public class HighScoreActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_score);
+        
+        try{
+            // Create a new HTTP Client
+            DefaultHttpClient defaultClient = new DefaultHttpClient();
+            // Setup the get request
+            HttpGet httpGetRequest = new HttpGet("http://game-details.com/tiltaball/highscore.php");
+
+            // Execute the request in the client
+            HttpResponse httpResponse = defaultClient.execute(httpGetRequest);
+            // Grab the response
+            BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent(), "UTF-8"));
+            String json = reader.readLine();
+
+            // Instantiate a JSON object from the request response
+            JSONObject jsonObject = new JSONObject(json);
+
+        } catch(Exception e){
+            // In your production code handle any errors and catch the individual exceptions
+            e.printStackTrace();
+        }
+
     }
 
 }
