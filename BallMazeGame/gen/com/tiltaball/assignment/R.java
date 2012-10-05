018cca9f4ce94eb8f45ef5809cac8d3776eb1cb1
Christian Holt <ymabob@gmail.com>
2012-10-05 03:05:13 +0200

    Of course something was missing...

--- BallMazeGame/gen/com/tiltaball/assignment/R.java
+++ BallMazeGame/gen/com/tiltaball/assignment/R.java
@@ -14,9 +14,8 @@ public final class R {
         public static final int how_to=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
-        public static final int ic_launcher_48x48=0x7f020003;
-        public static final int main_menu=0x7f020004;
-        public static final int title_screen=0x7f020005;
+        public static final int main_menu=0x7f020003;
+        public static final int title_screen=0x7f020004;
     }
     public static final class id {
         public static final int button_quit=0x7f060006;
